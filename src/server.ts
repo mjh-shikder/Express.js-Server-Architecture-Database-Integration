2a6a5fb82c34d7ba47b8b728f615dfb582a86f85
Md Jubair Hossain <mjhshikder.dhaka@gmail.com>
2026-05-21 16:39:33 +0600

    Neon Data base connected

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express,{ type Application, type Request, type Response }  from "express"
 
+import {Pool} from "pg" // eita ekta class
+
 
 const app: Application  = express();
 const port = 5000;
+
 
 //*---- Middleware---
 app.use(express.json());
@@ -11,6 +14,38 @@ app.use(express.text());
 app.use(express.urlencoded({extended: true})) 
 
 
+const pool = new Pool({
+  connectionString:
+    "postgresql://neondb_owner:@ep-blue-recipe-aq499ebk-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+});
+
+//* Database connecting function 
+const initDB = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(20), 
+      email VARCHAR(20) NOT NULL,
+      password VARCHAR(20) NOT NULL,
+      is_active BOOLEAN DEFAULT true,
+      age INT,
+      
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      
+      )
+      `);
+    
+    console.log("Database connected successfully");
+    
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+
+initDB()
 
 //* GET Method
 app.get("/", (req : Request , res : Response) => {
@@ -42,7 +77,7 @@ app.post('/', async (req : Request, res: Response) => {
 })
 
 
-//?--- Listener ----
+//? --- Listener ---
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
