006835914c254617357a6828063e34b01aaf8d9d
Md Jubair Hossain <mjhshikder.dhaka@gmail.com>
2026-05-21 20:33:53 +0600

    GET Single user and handle error

--- src/server.ts
+++ src/server.ts
@@ -120,6 +120,48 @@ app.get('/api/users', async (req: Request, res: Response) => {
   }
 
 })
+//*--------
+
+
+//*------ GET SINGLE USER ---------
+app.get('/api/users/:id', async (req: Request, res: Response) => {
+  try {
+    //? params thika id ta age ber korlam 
+    const id = req.params.id
+    // console.log(id);
+    
+    const result = await pool.query(`
+      SELECT * FROM users WHERE id=$1
+      `, [id]);
+    
+    //  console.log(result);
+    
+     if (result.rows.length === 0) {
+       res.status(500).json({
+         success: false,
+         message: "User Not Found",
+         data: {},
+       });
+     }
+
+    
+    res.status(200).json({
+      success: true,
+      message: "User Retrived Successfully",
+      data: result.rows[0],
+    });
+
+   
+    
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+})
+
 
 
 //? --- Listener ---
