1d6de4ac5c45c5a9b3f9162696d6f732c21d0e23
Md Jubair Hossain <mjhshikder.dhaka@gmail.com>
2026-05-21 12:44:44 +0600

    Middleware added, POST Methdo exploring

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,15 @@ import express,{ type Application, type Request, type Response }  from "express"
 const app: Application  = express();
 const port = 5000;
 
+//*---- Middleware---
+app.use(express.json());
+app.use(express.text());
+//? sudhu urlencoded Nested object recive kore na. O jeno nested data {name:{firstName:A, lastname:B}} o ney ei jonno extended : true dewa hoiche 
+app.use(express.urlencoded({extended: true})) 
+
+
+
+//* GET Method
 app.get("/", (req : Request , res : Response) => {
   // res.send("Express Server");
   res.status(200).json({
@@ -12,6 +21,15 @@ app.get("/", (req : Request , res : Response) => {
   })
 });
 
+
+//* POST Method
+app.post('/', async (req : Request, res: Response) => {
+  console.log(req.body);
+  
+})
+
+
+//?--- Listener ----
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
