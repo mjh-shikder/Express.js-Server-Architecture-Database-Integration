400c10033f004437d4d618412556b52d1a3d1f4a
Md Jubair Hossain <mjhshikder.dhaka@gmail.com>
2026-05-21 12:22:42 +0600

    send response as json

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,11 @@ const app: Application  = express();
 const port = 5000;
 
 app.get("/", (req : Request , res : Response) => {
-  res.send("Express Server");
+  // res.send("Express Server");
+  res.status(200).json({
+    "message": "Express Server",
+    "author": "mjh shikder"
+  })
 });
 
 app.listen(port, () => {
