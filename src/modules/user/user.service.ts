d68cf5a539c382b7e2250e47fb328d6cb00ab7a2
Md Jubair Hossain <mjhshikder.dhaka@gmail.com>
2026-05-25 16:24:21 +0600

    bcrypt hashing added to password

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,54 +1,56 @@
+import bcrypt from "bcryptjs";
 import { pool } from "../../db";
 import type { IUser } from "./user.interface";
 
 //* POST Create New User
 const createUserIntoDB = async (payload: IUser) => {
-    const { name, email, password, age }= payload
-     const result = await pool.query(
-       `
+  const { name, email, password, age } = payload;
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const result = await pool.query(
+    `
     INSERT INTO users(name, email, password, age)
     VALUES($1,$2,$3,$4)
     RETURNING *
     `,
-       [name, email, password, age],
+    [name, email, hashPassword, age],
     );
-    return result;
-}
+    
+    delete result.rows[0].password
 
-//* GET Get All Users 
+  return result;
+};
+
+//* GET Get All Users
 const getAllUsersFromDB = async () => {
-    const result = await pool.query(`
+  const result = await pool.query(`
       SELECT * FROM users
       `);
-    return result
-}
-
+  return result;
+};
 
 //* GET Single users
 
 const getSingleUserFromDB = async (id: string) => {
-    
-       
-       // console.log(id);
+  // console.log(id);
 
-       const result = await pool.query(
-         `
+  const result = await pool.query(
+    `
       SELECT * FROM users WHERE id=$1
       `,
-         [id],
-    );
-    
-    return result
-}
+    [id],
+  );
 
+  return result;
+};
 
 //* PUT Update user
 const updateUserFromDB = async (payload: IUser, id: string) => {
-    
-    const { name, password, age, is_active}= payload
+  const { name, password, age, is_active } = payload;
 
-    const result = await pool.query(
-      `
+  const result = await pool.query(
+    `
     UPDATE users 
     SET
     name=COALESCE($1, name),
@@ -59,30 +61,29 @@ const updateUserFromDB = async (payload: IUser, id: string) => {
     id= $5
     RETURNING *
     `,
-      [name, password, age, is_active, id],
-    );
+    [name, password, age, is_active, id],
+  );
 
-    return result
-}
+  return result;
+};
 
-//* DELETE User 
-const deleteUserFromDB = async (id:string) => {
-    const result = await pool.query(
-      `
+//* DELETE User
+const deleteUserFromDB = async (id: string) => {
+  const result = await pool.query(
+    `
       DELETE FROM users
       WHERE id= $1
       `,
-      [id],
-    );
-    return result
-}
-
+    [id],
+  );
+  return result;
+};
 
 //?------EXPORT----------
 export const userService = {
-    createUserIntoDB,
-    getAllUsersFromDB,
-    getSingleUserFromDB,
-    updateUserFromDB,
-    deleteUserFromDB,
-}
+  createUserIntoDB,
+  getAllUsersFromDB,
+  getSingleUserFromDB,
+  updateUserFromDB,
+  deleteUserFromDB,
+};
