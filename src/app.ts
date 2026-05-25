c514a84cdb9fada49229a76af9ff514e930417be
Md Jubair Hossain <mjhshikder.dhaka@gmail.com>
2026-05-25 15:14:22 +0600

    profile module added and managing profile with sql reference

--- src/app.ts
+++ src/app.ts
@@ -4,20 +4,16 @@ import express, {
   type Response,
 } from "express";
 
-
-
 import { userRoute } from "./modules/user/user.route";
+import { profileRoute } from "./modules/profile/profile.route";
 
 const app: Application = express();
-
 
 //*---- Middleware---
 app.use(express.json());
 app.use(express.text());
 //? sudhu urlencoded Nested object recive kore na. O jeno nested data {name:{firstName:A, lastname:B}} o ney ei jonno extended : true dewa hoiche
 app.use(express.urlencoded({ extended: true }));
-
-
 
 //* Root Route
 app.get("/", (req: Request, res: Response) => {
@@ -36,9 +32,7 @@ app.get("/", (req: Request, res: Response) => {
 // }
 
 //? All Method with router
-app.use('/api/users', userRoute)
+app.use("/api/users", userRoute);
+app.use("/api/profile", profileRoute);
 
-
-
-export default app
-
+export default app;
--- src/db/index.ts
+++ src/db/index.ts
@@ -23,6 +23,19 @@ export const initDB = async () => {
       )
       `);
     
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS profiles(
+      id SERIAL PRIMARY KEY,
+      user_id INT UNIQUE REFERENCES users(id) ON DELETE CASCADE,
+      bio TEXT,
+      address TEXT,
+      phone VARCHAR(11),
+      gender VARCHAR(10),
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )
+      `);
+    
     console.log("Database connected successfully");
     
   } catch (error) {
