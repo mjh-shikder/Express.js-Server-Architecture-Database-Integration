4143b957cb1c4808355ae94fd4e1628f17ab32e9
Md Jubair Hossain <mjhshikder.dhaka@gmail.com>
2026-06-02 07:15:36 +0600

    set user to req and namespace

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -4,46 +4,59 @@ import config from "../config";
 import { pool } from "../db";
 const auth = () => {
     return async (req: Request, res: Response, next: NextFunction) => {
-     // console.log("this is protected route");
-     //   console.log(req.headers.authorization);
-        const token = req.headers.authorization 
-        
-        if (!token) {
-            res.status(401).json({
-                sucess: false,
-                message: "Unauthorized access!"
-            })
-        }
+    try {
+      // console.log("this is protected route");
+      //   console.log(req.headers.authorization);
+      const token = req.headers.authorization;
 
-        const decoded = jwt.verify(token as string, config.secret as string) as JwtPayload;
+      if (!token) {
+        res.status(401).json({
+          sucess: false,
+          message: "Unauthorized access!",
+        });
+      }
 
-       // console.log(decoded);
-        
-        const userData = await pool.query(`
-            SELECT * FROM users WHERE email = $1`, [decoded.email])
-        
-        
-        // now lets dife the user 
-        const user = userData.rows[0]
-        // console.log(user);
-        
-        //? ei validation ta ensure korbe jodi user DB theke delete kora hoy er pore o jeno data na dey 
-        if (userData.rows.length === 0) {
-            res.status(404).json({
-              sucess: false,
-              message: "User not found!",
-            });
-        }
+      const decoded = jwt.verify(
+        token as string,
+        config.secret as string,
+      ) as JwtPayload;
 
-        //? user active validation 
-        if (!user.is_active) {
-            res.status(403).json({
-              sucess: false,
-              message: "Forbidden access!",
-            }); 
-        }
-        
+      // console.log(decoded);
+
+      const userData = await pool.query(
+        `
+            SELECT * FROM users WHERE email = $1`,
+        [decoded.email],
+      );
+
+      // now lets dife the user
+      const user = userData.rows[0];
+      // console.log(user);
+
+      //? ei validation ta ensure korbe jodi user DB theke delete kora hoy er pore o jeno data na dey
+      if (userData.rows.length === 0) {
+        res.status(404).json({
+          sucess: false,
+          message: "User not found!",
+        });
+      }
+
+      //? user active validation
+      if (!user.is_active) {
+        res.status(403).json({
+          sucess: false,
+          message: "Forbidden access!",
+        });
+      }
+
+        //? ei khane reqest er moddhe user ta set kora holo 
+      req.user = decoded;
+
       next();
+    } catch (error) {
+
+        next(error)
+    }
     };
 }
 
